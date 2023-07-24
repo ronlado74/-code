1c43532bff1cc3fff2819ff4b0da07effc42cce3
wangxingyu <xingyuwang14530@163.com>
2023-07-25 00:00:26 +0800

    feat: setTimeOut中this指向

--- js_learn/一些面试题/setTimeOut中this指向的问题.js
+++ js_learn/一些面试题/setTimeOut中this指向的问题.js
@@ -1,5 +1,11 @@
 let test = {
-  a: function () {
+  a1: function () {
     setTimeout(function () { console.log(this, '111111') }, 1000)
+  },
+  a2: function () {
+    setTimeout(() => { console.log(this, '111111') }, 1000)
   }
 }
+
+test.a1() // Window
+test.a2() // test
